2b4d801d90a2734ef158f50312cb14f9dec93737
niladri <niladri83@gmail.com>
2017-04-25 15:14:38 +0530

    PD controller

--- pdcontroller.py
+++ pdcontroller.py
@@ -1,23 +1,20 @@
 # -----------
 # User Instructions
 #
-# Implement a P controller by running 100 iterations
-# of robot motion. The desired trajectory for the 
-# robot is the x-axis. The steering angle should be set
+# Implement a PD controller by running 100 iterations
+# of robot motion. The steering angle should be set
 # by the parameter tau so that:
 #
-# steering = -tau * crosstrack_error
-#
-# Note that tau is called "param" in the function
-# below.
+# steering = -tau_p * CTE - tau_d * diff_CTE
+# where differential crosstrack error (diff_CTE)
+# is given by CTE(t) - CTE(t-1)
 #
 # Your code should print output that looks like
-# the output shown in the video. That is, at each step:
-# print myrobot, steering
+# the output shown in the video.
 #
 # Only modify code at the bottom!
 # ------------
-
+ 
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -113,25 +110,41 @@ class Robot(object):
 # ------------------------------------------------------------------------
 #
 # run - does a single control run
+
+# previous P controller
+def run_p(robot, tau, n=100, speed=1.0):
+    x_trajectory = []
+    y_trajectory = []
+    for i in range(n):
+        cte = robot.y
+        steer = -tau * cte
+        robot.move(steer, speed)
+        x_trajectory.append(robot.x)
+        y_trajectory.append(robot.y)
+    return x_trajectory, y_trajectory
+    
 robot = Robot()
 robot.set(0, 1, 0)
 
-def run(robot, tau, n=100, speed=1.0):
+def run(robot, tau_p, tau_d, n=100, speed=1.0):
     x_trajectory = []
     y_trajectory = []
-    # TODO: your code here
-    x_trajectory.append(robot.x)
-    y_trajectory.append(robot.y)    
+    prevCTE = robot.y
     for i in range(n):
-        steering = (-1.0) * tau * robot.y
-        robot.move(steering, 1.0)
+        cte = robot.y
+        dcte = cte - prevCTE
+        steer = -tau_p * cte -tau_d * dcte 
+        robot.move(steer, speed)
         x_trajectory.append(robot.x)
-        y_trajectory.append(robot.y)        
+        y_trajectory.append(robot.y)
+        prevCTE = cte
+        #print(robot, steer)
     return x_trajectory, y_trajectory
+    
+x_trajectory, y_trajectory = run(robot, 0.2, 3.0)
 
-x_trajectory, y_trajectory = run(robot, 0.1)
 n = len(x_trajectory)
-
-plt.plot(x_trajectory, y_trajectory, 'g', label='proportional controller')
+plt.plot(x_trajectory, y_trajectory, 'g', label='PD controller')
 plt.plot(x_trajectory, np.zeros(n), 'r', label='reference')
+plt.legend()
 plt.show()
