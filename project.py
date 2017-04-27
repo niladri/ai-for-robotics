eb370993450ba486f850971021c152e28a4285f4
niladri <niladri83@gmail.com>
2017-04-27 22:47:51 +0530

    Robot tracking with noise

--- project.py
+++ project.py
@@ -22,27 +22,21 @@
 # plan for recovering the lost Traxbot.
 # 
 # ----------
-# Part One
+# Part Two
 #
-# Let's start by thinking about circular motion (well, really it's polygon motion
-# that is close to circular motion). Assume that Traxbot lives on 
-# an (x, y) coordinate plane and (for now) is sending you PERFECTLY ACCURATE sensor 
-# measurements. 
-#
-# With a few measurements you should be able to figure out the step size and the 
-# turning angle that Traxbot is moving with.
-# With these two pieces of information, you should be able to 
-# write a function that can predict Traxbot's next location.
-#
-# You can use the robot class that is already written to make your life easier. 
-# You should re-familiarize yourself with this class, since some of the details
-# have changed. 
+# Now we'll make the scenario a bit more realistic. Now Traxbot's
+# sensor measurements are a bit noisy (though its motions are still
+# completetly noise-free and it still moves in an almost-circle).
+# You'll have to write a function that takes as input the next
+# noisy (x, y) sensor measurement and outputs the best guess 
+# for the robot's next position.
 #
 # ----------
 # YOUR JOB
 #
-# Complete the estimate_next_pos function. You will probably want to use
-# the OTHER variable to keep track of information about the runaway robot.
+# Complete the function estimate_next_pos. You will be considered 
+# correct if your estimate is within 0.01 stepsizes of Traxbot's next
+# true position. 
 #
 # ----------
 # GRADING
@@ -60,23 +54,37 @@ from math import *
 from matrix import *
 import random
 
-# Using Kalman 4D
+# Using Kalman 6D
+F =  matrix([[1.0, 0.0, 1.0, 0.0, 0.5, 0.0, 0.0, 0.0], 
+             [0.0, 1.0, 0.0, 1.0, 0.0, 0.5, 0.0, 0.0],
+             [0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.5, 0.0],
+             [0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.5],
+             [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0],
+             [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0], 
+             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 
+             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+# covariance matrix
+q = F * F.transpose() * matrix([[0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+             [0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+             [0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0, 0.0],
+             [0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0, 0.0],
+             [0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0, 0.0],
+             [0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0, 0.0], 
+             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01, 0.0], 
+             [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.01]])
+# next state function
 
-u = matrix([[0.], [0.], [0.], [0.]]) # external motion 
-dt = 1.0
-F =  matrix([[1., 0., dt, 0.], [0., 1., 0., dt], [0., 0., 1., 0.], [0., 0., 0., 1.]])
-# next state function: generalize the 2d version to 4d
-
-H =  matrix([[1., 0., 0., 0.], [0., 1., 0., 0.]])
+H =  matrix([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]])
 # measurement function: reflect the fact that we observe x and y but not the two velocities
 
-R =  matrix([[0.00001, 0.0], [0.0, 0.00001]])
-# measurement uncertainty: use 2x2 matrix with 0.0 as main diagonal due to no uncertainty
-
-I =  matrix([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
-# 4d identity matrix
+R =  matrix([[0.05, 0.0], [0.0, 0.05]])
+# measurement uncertainty: use 2x2 matrix with 0.05 as main diagonal
 
 def kalman_filter(x, P, measurement):
+    
+    dim = x.dimx
+    I = matrix([[]])
+    I.identity(dim)
     
     # measurement update
     Z = matrix([measurement])
@@ -87,13 +95,13 @@ def kalman_filter(x, P, measurement):
     P = (I - (K * H)) * P   
     
     # prediction
-    x = (F * x) + u
-    P = F * P * F.transpose()     
+    x = F * x
+    P = F * P * F.transpose() + q
     
-    print('x= ')
-    x.show()
-    print('P= ')
-    P.show()
+    #print('x= ')
+    #x.show()
+    #print('P= ')
+    #P.show()
     
     return x, P
     
@@ -107,8 +115,15 @@ def estimate_next_pos(measurement, OTHER = None):
     """Estimate the next (x, y) position of the wandering Traxbot
     based on noisy (x, y) measurements."""
 
-    x = matrix([[0.], [0.], [0.], [0.]]) # initial state (location and velocity)      
-    P =  matrix([[0., 0., 0., 0.], [0., 0., 0., 0.], [0., 0., 1000., 0.], [0., 0., 0., 1000.]]) 
+    x = matrix([[0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0], [0.0]]) # initial state (location, velocity and acceleration)      
+    P =  matrix([[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+                 [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+                 [0.0, 0.0, 1000.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+                 [0.0, 0.0, 0.0, 1000.0, 0.0, 0.0, 0.0, 0.0], 
+                 [0.0, 0.0, 0.0, 0.0, 1000.0, 0.0, 0.0, 0.0],
+                 [0.0, 0.0, 0.0, 0.0, 0.0, 1000.0, 0.0, 0.0], 
+                 [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1000.0, 0.0], 
+                 [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1000.0]]) 
     # initial uncertainty: 0 for positions x and y, 1000 for the two velocities
     
     # You must return xy_estimate (x, y), and OTHER (even if it is None) 
@@ -118,33 +133,68 @@ def estimate_next_pos(measurement, OTHER = None):
     state = []
     if not OTHER: # this is the first measurement  
         # only prediction
-        #x = (F * x) + u
+        #x = F * x
         #P = F * P * F.transpose() 
         #OTHER = []
         #OTHER.append(x)
         #OTHER.append(P)
+        #xy_estimate = measurement
         xy_estimate = measurement
         orientation_estimate = atan2(measurement[1], measurement[0])
         state.append(measurement)
         state.append(orientation_estimate)
+        state.append(0) # count
     else:
         # measure and then predict
         #x, P = kalman_filter(OTHER[0], OTHER[1], measurement)
         #OTHER[0] = x
         #OTHER[1] = P
+        #xy_estimate = x.value[0][0], x.value[1][0]
         previous_measurement = OTHER[0]
         dx = measurement[0] - previous_measurement[0]
         dy = measurement[1] - previous_measurement[1]
         orientation_current = atan2(dy, dx)
         orientation_previous = OTHER[1]
+        #print("c: ", measurement, "p: ", previous_measurement)
+        #print("dx: ", dx, "dy: ", dy)
+        #print("o_c:", orientation_current, "o_p: ", orientation_previous)
         dtheta = orientation_current - orientation_previous
+        if dtheta < -pi/2:
+            dtheta += 2 * pi
         d = distance_between(measurement, previous_measurement)
+        #print("d: ", d, "dtheta: ", dtheta)
         orientation_estimate = orientation_current + dtheta
-        x_estimate = measurement[0] + d * cos(orientation_estimate)
-        y_estimate = measurement[1] + d * sin(orientation_estimate)
-        xy_estimate = x_estimate, y_estimate
-        state.append(measurement)
-        state.append(orientation_current)
+        count = OTHER[2]
+        if count == 0:
+            dList = [d]
+            wList = [dtheta]
+            x_estimate = measurement[0] + d * cos(orientation_estimate)
+            y_estimate = measurement[1] + d * sin(orientation_estimate)
+            xy_estimate = x_estimate, y_estimate
+            state.append(measurement)
+            state.append(orientation_current)  
+            state.append(count+1)
+            state.append(dList)
+            state.append(wList)            
+        else:
+            dList = OTHER[3]
+            wList = OTHER[4]
+            if not len(dList) == len(wList):
+                raise(ValueError, "List of past d and theta should have equal lengths")
+            d_weighted = (sum(dList) + d) / (len(dList) + 1)
+            dtheta_weighted = (sum(wList) + dtheta) / (len(wList) + 1)
+            #print("d_w:", d_weighted, "t_w:", dtheta_weighted)
+            orientation_estimate = orientation_current + dtheta_weighted
+            x_estimate = measurement[0] + d_weighted * cos(orientation_estimate)
+            y_estimate = measurement[1] + d_weighted * sin(orientation_estimate)
+            xy_estimate = x_estimate, y_estimate
+            dList.append(d)
+            wList.append(dtheta)
+            state.append(measurement)
+            state.append(orientation_current)  
+            state.append(count+1)
+            state.append(dList)
+            state.append(wList)             
     OTHER = state
     return xy_estimate, OTHER    
 
@@ -165,19 +215,76 @@ def demo_grading(estimate_next_pos_fcn, target_bot, OTHER = None):
     # if you haven't localized the target bot, make a guess about the next
     # position, then we move the bot and compare your guess to the true
     # next position. When you are close enough, we stop checking.
-    while not localized and ctr <= 10: 
+    while not localized and ctr <= 1000:
         ctr += 1
         measurement = target_bot.sense()
         position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
         target_bot.move_in_circle()
         true_position = (target_bot.x, target_bot.y)
-        print(true_position, position_guess)
         error = distance_between(position_guess, true_position)
+        print("error: ", error, " tolerance: ", distance_tolerance)
         if error <= distance_tolerance:
             print("You got it right! It took you ", ctr, " steps to localize.")
             localized = True
-        if ctr == 10:
+        if ctr == 1000:
             print("Sorry, it took you too many steps to localize the target.")
+    return localized
+
+def demo_grading_v(estimate_next_pos_fcn, target_bot, OTHER = None):
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+    # if you haven't localized the target bot, make a guess about the next
+    # position, then we move the bot and compare your guess to the true
+    # next position. When you are close enough, we stop checking.
+    #For Visualization
+    import turtle    #You need to run this locally to use the turtle module
+    window = turtle.Screen()
+    window.bgcolor('white')
+    size_multiplier= 25.0  #change Size of animation
+    broken_robot = turtle.Turtle()
+    broken_robot.shape('turtle')
+    broken_robot.color('green')
+    broken_robot.resizemode('user')
+    broken_robot.shapesize(0.1, 0.1, 0.1)
+    measured_broken_robot = turtle.Turtle()
+    measured_broken_robot.shape('circle')
+    measured_broken_robot.color('red')
+    measured_broken_robot.resizemode('user')
+    measured_broken_robot.shapesize(0.1, 0.1, 0.1)
+    prediction = turtle.Turtle()
+    prediction.shape('arrow')
+    prediction.color('blue')
+    prediction.resizemode('user')
+    prediction.shapesize(0.1, 0.1, 0.1)
+    prediction.penup()
+    broken_robot.penup()
+    measured_broken_robot.penup()
+    #End of Visualization
+    while not localized and ctr <= 1000:
+        ctr += 1
+        measurement = target_bot.sense()
+        position_guess, OTHER = estimate_next_pos_fcn(measurement, OTHER)
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        #print("error: ", error, " tolerance: ", distance_tolerance)
+        if error <= distance_tolerance:
+            print("You got it right! It took you ", ctr, " steps to localize.")
+            localized = True
+        if ctr == 1000:
+            print("Sorry, it took you too many steps to localize the target.")
+        #More Visualization
+        measured_broken_robot.setheading(target_bot.heading*180/pi)
+        measured_broken_robot.goto(measurement[0]*size_multiplier, measurement[1]*size_multiplier-200)
+        measured_broken_robot.stamp()
+        broken_robot.setheading(target_bot.heading*180/pi)
+        broken_robot.goto(target_bot.x*size_multiplier, target_bot.y*size_multiplier-200)
+        broken_robot.stamp()
+        prediction.setheading(target_bot.heading*180/pi)
+        prediction.goto(position_guess[0]*size_multiplier, position_guess[1]*size_multiplier-200)
+        prediction.stamp()
+        #End of Visualization
     return localized
 
 # This is a demo for what a strategy could look like. This one isn't very good.
@@ -193,6 +300,7 @@ def naive_next_pos(measurement, OTHER = None):
 # This is how we create a target bot. Check the robot.py file to understand
 # How the robot class behaves.
 test_target = robot(2.1, 4.3, 0.5, 2*pi / 34.0, 1.5)
-test_target.set_noise(0.0, 0.0, 0.0)
+measurement_noise = 0.05 * test_target.distance
+test_target.set_noise(0.0, 0.0, measurement_noise)
 
 demo_grading(estimate_next_pos, test_target)
